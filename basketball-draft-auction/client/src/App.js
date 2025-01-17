b18a9a09b2d0a4c072539e3397efcefe47f0ef34
KhizerUrRehman <35479361+KhizerUrRehman@users.noreply.github.com>
2025-01-17 11:26:43 +0500

    Update App.js

--- basketball-draft-auction/client/src/App.js
+++ basketball-draft-auction/client/src/App.js
@@ -9,8 +9,6 @@ import PlayerPool from './pages/PlayerPool';
 import LogPage from "./components/LogPage"; // New log page
 import logo from "./assets/lob.jpg"; // Import the logo
 
-/*
-
 function App() {
   return ( 
     <Router>
@@ -18,16 +16,12 @@ function App() {
         <nav className="navbar">  
   <ul className="nav-links">
     <li><a href="/home">Home</a></li>
-    <li><a href="/auction">Auction</a></li>
     <li><a href="/captains">Captains</a></li>
     <li><a href="/player-pool">Player Pool</a></li>
-    <li><a href="/draft-master">Draft Master</a></li>
   </ul>
   <h1 className="navbar-title">RBL AUCTION</h1>
   <img src={logo} alt="Company Logo" className="logo" />
 </nav> 
-
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auction" element={<AuctionScreen />} />
@@ -41,38 +35,4 @@ function App() {
     </Router>
   );
 }
-
-*/
-
-function App() {
-  return ( 
-    <Router>
-      <div className="App">
-        <nav className="navbar">  
-  <ul className="nav-links">
-    <li><Link to="/home">Home</Link></li>
-    <li><Link to="/captains">Captains</Link></li>
-    <li><Link to="/auction">Auction</Link></li>
-    <li><Link to="/player-pool">Player Pool</Link></li>
-    <li><Link to="/draft-master">Draft Master</Link></li>
-  </ul>
-  <h1 className="navbar-title">RBL AUCTION</h1>
-  <img src={logo} alt="Company Logo" className="logo" />
-</nav> 
-
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auction" element={<AuctionScreen />} />
-          <Route path="/captains" element={<Captains />} />
-          <Route path="/player-pool" element={<PlayerPool />} />
-          <Route path="/draft-master" element={<DraftMaster />} />
-          <Route path="/logs" element={<LogPage />} /> 
-          <Route path="/home" element={<Home />} /> 
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
 export default App;
