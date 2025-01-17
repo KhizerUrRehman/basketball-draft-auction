13258b67d57b823d60c3a04f99a0faa6828121fa
KhizerUrRehman <35479361+KhizerUrRehman@users.noreply.github.com>
2025-01-17 11:27:36 +0500

    Update Home.jsx

--- basketball-draft-auction/client/src/pages/Home.jsx
+++ basketball-draft-auction/client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div>
       <h1>Welcome to the RBL Draft Auction!</h1>
-      <p>Select Captains Dashboard to see current screen, select Player Pool to see remaining players. Select log to see events.</p>
+      <p>Select Captains Dashboard to see current screen, select Player Pool to see remaining players.</p>
     </div>
   );
 }
