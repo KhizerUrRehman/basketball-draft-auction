166365edac0e984df93dbf282ca3f2016b2a6793
KhizerUrRehman <35479361+KhizerUrRehman@users.noreply.github.com>
2025-01-17 19:44:01 +0500

    Home screen image

--- basketball-draft-auction/client/src/pages/Home.jsx
+++ basketball-draft-auction/client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import rbl from "./assets/rbl.jpg";
 
 function Home() {
   return (
     <div>
       <h1>Welcome to the RBL Draft Auction!</h1>
       <p>Select Captains Dashboard to see current screen, select Player Pool to see remaining players.</p>
+      <img src={rbl} alt="RBL Champs" className="rbl" />
     </div>
   );
 }
