4d138151996d5211791c345ff9efe174e93b3f98
KhizerUrRehman <35479361+KhizerUrRehman@users.noreply.github.com>
2025-01-18 09:27:07 +0500

    Update AuctionScreen.jsx

--- basketball-draft-auction/client/src/components/AuctionScreen.jsx
+++ basketball-draft-auction/client/src/components/AuctionScreen.jsx
@@ -238,11 +238,9 @@ const AuctionScreen = () => {
 
       <div className="auction-logs">
         <h3>Auction Logs</h3>
-        <ul>
           {[...logs].reverse().map((log, index) => (
             <li key={index}>{log}</li>
           ))}
-        </ul>
       </div>
     </div>
   );
